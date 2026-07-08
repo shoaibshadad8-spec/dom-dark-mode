6fd22de9dbe40e33f36132c7939570436af033c0
shoaib shadad <shoaibshadad8@gmail.com>
2026-07-08 15:43:37 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+      
+      /*function goBack() {
+        //history.go(-1);
+        history.back();
+      }
+      function goForward() {
+        //history.go(1);
+        history.forward();
+      }
+      function goBackTwo() {
+        // Go back 2 pages
+        history.go(-2);
+      }
+      function reloadPage() {
+        // Reload current page
+        history.go(0);
+      }
+      function showLength() {
+        document.getElementById("InfoBox").innerHTML =
+          `<span class="label">History Length:</span> ${history.length}`;
+      }*/
